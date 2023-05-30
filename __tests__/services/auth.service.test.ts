5699517fbac72865ec95d335c2b76f369f312b89
Fabio Flores <fabioflores@ravn.com>
2023-05-29 19:20:16 -0600

    test: add test auth service

---
+++ __tests__/services/auth.service.test.ts
@@ -0,0 +1,43 @@
+import { User } from '@prisma/client';
+import jwt from 'jsonwebtoken';
+import { AuthService } from '../../src/services/auth.service';
+import { PartialMock } from '../utils/generic';
+import UserService from '../../src/services/user.service';
+
+describe('AuthService', () => {
+  const user = {
+    id: 1,
+    role: 'USER',
+  } as User;
+  let userService: PartialMock<UserService> = {
+    update: jest.fn(),
+  };
+  const authService = new AuthService(userService as unknown as UserService);
+  const token = 'this a example token';
+  const spySign = jest.spyOn(jwt, 'sign').mockImplementation(() => token);
+
+  describe('signToken', () => {
+    it('should return new sign token', async () => {
+      expect.assertions(2);
+
+      const actual = authService.signToken(user);
+
+      expect(actual).toEqual(token);
+      expect(spySign).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('createAccessToken', () => {
+    it('should return accessToken and update user', async () => {
+      expect.assertions(3);
+
+      const spyUserService = jest.spyOn(userService, 'update');
+
+      const actual = await authService.createAccessToken(user);
+
+      expect(actual).toEqual(token);
+      expect(spySign).toBeCalledTimes(1);
+      expect(spyUserService).toHaveBeenCalledTimes(1);
+    });
+  });
+});
--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,11 +1,12 @@
 import config from '../config';
 import { PayloadJwt } from '../types/generic';
 import jwt from 'jsonwebtoken';
-import { userService } from '../dependencies/dependencies';
 import { hashSync } from 'bcrypt';
 import { User } from '@prisma/client';
+import UserService from './user.service';
 
 export class AuthService {
+  constructor(private readonly userService: UserService) {}
   signToken(user: User): string {
     const payload: PayloadJwt = {
       sub: user.id,
@@ -15,11 +16,11 @@ export class AuthService {
     return accessToken;
   }
 
-  async createAccessToken(user: User) {
+  async createAccessToken(user: User): Promise<string> {
     const accessToken = this.signToken(user);
-    await userService.update(user.id, {
+    await this.userService.update(user.id, {
       accessToken: hashSync(accessToken, 10),
     });
-    return { accessToken };
+    return accessToken;
   }
 }
