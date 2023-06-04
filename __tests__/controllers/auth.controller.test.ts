c0ff67d626d550adc6d390fea2030a8c42323a3d
fabioflores <fabioflores021@gmail.com>
2023-06-04 16:47:37 -0600

    test: add test logout auth controller

--- __tests__/controllers/auth.controller.test.ts
+++ __tests__/controllers/auth.controller.test.ts
@@ -17,16 +17,17 @@ describe('AuthController', () => {
   let authController: AuthController;
   const res = buildRes();
   const next = buildNext();
-  const user = buildUser({
-    sub: getId,
-    isVerify: true,
-  }) as User;
-  const req = buildReq({ user }) as unknown as Request;
   const successfulStatus = 200;
-  const accessToken = getToken;
   const error = new Error('unexpected error');
 
   describe('login', () => {
+    const user = buildUser({
+      sub: getId,
+      isVerify: true,
+    }) as User;
+    const req = buildReq({ user }) as unknown as Request;
+    const accessToken = getToken;
+
     it('should create an access token', async () => {
       mockAuthService = {
         createAccessToken: jest.fn().mockResolvedValueOnce(accessToken),
@@ -62,6 +63,50 @@ describe('AuthController', () => {
       expect(res.json).not.toHaveBeenCalled();
       expect(res.status).not.toHaveBeenCalled();
       expect(mockAuthService.createAccessToken).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('logout', () => {
+    const req = buildReq({
+      headers: { authorization: `Bearer ${getToken}` },
+    }) as unknown as Request;
+    const response = 'logout successfully';
+
+    it('should logout account', async () => {
+      mockAuthService = {
+        logout: jest.fn().mockResolvedValueOnce(response),
+      };
+      authController = new AuthController(
+        mockAuthService as unknown as AuthService,
+      );
+      const expected = { message: response };
+
+      await authController.logout(req, res, next);
+
+      expect(res.json).toHaveBeenCalledTimes(1);
+      expect(res.json).toHaveBeenCalledWith(expected);
+      expect(res.status).toHaveBeenCalledTimes(1);
+      expect(res.status).toHaveBeenCalledWith(successfulStatus);
+      expect(mockAuthService.logout).toHaveBeenCalledTimes(1);
+    });
+
+    it('when happen an error invoke next funtion with the error', async () => {
+      expect.assertions(5);
+      mockAuthService = {
+        logout: jest.fn().mockRejectedValueOnce(error),
+      };
+
+      authController = new AuthController(
+        mockAuthService as unknown as AuthService,
+      );
+
+      await authController.logout(req, res, next);
+
+      expect(next).toHaveBeenCalledTimes(1);
+      expect(next).toHaveBeenCalledWith(error);
+      expect(res.json).not.toHaveBeenCalled();
+      expect(res.status).not.toHaveBeenCalled();
+      expect(mockAuthService.logout).toHaveBeenCalledTimes(1);
     });
   });
 });
